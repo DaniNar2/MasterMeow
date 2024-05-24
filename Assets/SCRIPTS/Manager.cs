08ff0293aae9226ae5a03294c31e4fa22f696248
Dani <daniela.nardone106@gmail.com>
2024-05-24 12:03:49 +0200

    Import Unity Project

---
+++ Assets/SCRIPTS/Manager.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+    [SerializeField] MasterMeow masterMeow;
+    [SerializeField] GameObject[] gameSlot = new GameObject[10];
+    [SerializeField] GameObject[] gameVerify = new GameObject[4];  
+    private int currentSlot = 0, currentCol = 1;
+    private Sprite emptySprite;
+    [SerializeField] string[] code = new string[4];
+
+    void Start()
+    {
+        masterMeow.GetNewSecretCode();
+        emptySprite = gameSlot[currentSlot].transform.Find("C1").GetComponent<Image>().sprite;
+    }
+
+    public void ColorSelect(Sprite sp)
+    {
+        if(!masterMeow.hiddenSlot.activeInHierarchy) return;
+
+        gameSlot[currentSlot].transform.Find("C" + currentCol).GetComponent<Image>().sprite = sp;
+        code.SetValue(sp.name, currentCol -1);
+        currentCol++;
+        if(currentCol == 5) currentCol = 1;
+    }
+
+    public void Cancel()
+    {
+        for(int i = 1; i < 5; i++)
+        {
+            gameSlot[currentSlot].transform.Find("C" + i).GetComponent<Image>().sprite = emptySprite;
+        }
+        currentCol = 1;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Check()
+    {
+       if(!masterMeow.hiddenSlot.activeInHierarchy) return;
+
+       int x = 0;
+       foreach(Transform child in gameSlot[currentSlot].transform)
+       {
+            if(child.tag == "Verify")
+            {
+                gameVerify[x] = child.gameObject;
+                x++;
+            }
+       }
+
+       for(int i = 1; i < 5; i++)
+       {
+        if(gameSlot[currentSlot].transform.Find("C" + i).GetComponent<Image>().sprite == emptySprite) return;
+       }
+
+       int nbGoodPosition = masterMeow.GetGoodPosition(code);
+       for(int i = 0; i < nbGoodPosition; i++)
+       {
+        gameVerify[i].GetComponent<Image>().sprite = masterMeow.Good;
+       }
+
+       int nbWrongPosition = masterMeow.GetWrongPosition();
+       for(int i = nbGoodPosition; i < nbWrongPosition + nbGoodPosition; i++)
+       {
+        gameVerify[i].GetComponent<Image>().sprite = masterMeow.Wrong;
+       }
+
+       if(nbGoodPosition == 4)
+       {
+        Debug.Log("WINNER!");
+        Win();
+        return;
+       }
+
+       if(currentSlot == 9)
+       {
+        Debug.Log("LOSER!");
+        Lose();
+        return;
+       }
+
+       currentSlot++;
+
+       Color original = gameSlot[currentSlot].GetComponent<Image>().color;
+       Color selected = original;
+       selected.a = 0.25f;
+
+       gameSlot[currentSlot].GetComponent<Image>().color = selected;
+       gameSlot[currentSlot-1].GetComponent<Image>().color = original;
+    }
+
+    void Win()
+    {
+        masterMeow.hiddenSlot.SetActive(false);
+    }
+
+    void Lose()
+    {
+        masterMeow.hiddenSlot.SetActive(false);
+    }
+}
---
+++ Assets/SCRIPTS/MasterMeow.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+using UnityEngine.UI;
+
+public class MasterMeow : MonoBehaviour
+{
+    public Sprite Cat, Fish, Hippo, Dog, Frog, Horse, Good, Wrong;
+    public string[] secretCode = new string[4];
+    public string[] secretCodeTemp = new string[4];
+    private Dictionary<string, Sprite> dicoSprite = new Dictionary<string, Sprite>();
+    private string[] codePlayer = new string[4];
+    public GameObject hiddenSlot;
+    void Awake()
+    {
+        dicoSprite.Add("Cat", Cat);
+        dicoSprite.Add("Fish", Fish);
+        dicoSprite.Add("Hippo", Hippo);
+        dicoSprite.Add("Dog", Dog);
+        dicoSprite.Add("Frog", Frog);
+        dicoSprite.Add("Horse", Horse);
+    }
+    public Array GetNewSecretCode()
+    {
+        for (int i = 0; i<4; i++)
+        {
+            int rnd = UnityEngine.Random.Range(0, dicoSprite.Count);
+            secretCode.SetValue(dicoSprite.ElementAt(rnd).Key, i);
+        }
+
+        transform.Find("C1").GetComponent<Image>().sprite = dicoSprite[secretCode.GetValue(0).ToString()];
+        transform.Find("C2").GetComponent<Image>().sprite = dicoSprite[secretCode.GetValue(1).ToString()];
+        transform.Find("C3").GetComponent<Image>().sprite = dicoSprite[secretCode.GetValue(2).ToString()];
+        transform.Find("C4").GetComponent<Image>().sprite = dicoSprite[secretCode.GetValue(3).ToString()];
+
+        return secretCode;
+    }
+
+
+    public int GetGoodPosition(string[] code)
+    {   
+        Array.Copy(secretCode, secretCodeTemp, secretCode.Length);
+        int good = 0;
+        for(int i = 0; i < secretCodeTemp.Length; i++)
+        {
+            if(code[i] == secretCodeTemp[i])
+            {
+                good++;
+                code[i] = "good";
+                secretCodeTemp[i] = "good";
+            }
+        }
+        Array.Copy(code, codePlayer, code.Length);
+        return good;
+
+    }
+
+    public int GetWrongPosition()
+    {
+        int wrong = 0;
+        for(int i = 0; i < codePlayer.Length; i++)
+        {
+            for(int j = 0; j < secretCodeTemp.Length; j++)
+            {
+                if(codePlayer[i] == secretCodeTemp[j] && codePlayer[i]!="good" && secretCodeTemp[j]!="good")
+                {
+                    secretCodeTemp[j] = "wrong";
+                    wrong++;
+                    break;
+                }
+            }
+        }
+        return wrong;
+    }
+}
