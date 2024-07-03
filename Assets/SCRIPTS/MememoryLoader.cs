80a925d83ed094964981d6b9d2929b0a2ae5592b
SofMel <sofia.melcarne16@gmail.com>
2024-07-04 00:37:56 +0200

    MEMEMORY REFACTORING: SHUFFLE FUNCTION

--- Assets/SCRIPTS/MememoryLoader.cs
+++ Assets/SCRIPTS/MememoryLoader.cs
@@ -1,45 +1,74 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class MememoryLoader : MonoBehaviour
+public class MemoryLoader : MonoBehaviour
 {
-    [SerializeField] List<Sprite> lstItem = new List<Sprite>();
-    private GameObject[] slot; //funziona con serializefield
+    public List<Sprite> spriteList = new List<Sprite>(); 
+    [SerializeField] AudioClip sfxFound, sfxReturn, flipSound;
+    private GameObject[] pads;
+    private AudioSource audioSource;
 
-
-    // Start is called before the first frame update
     void Awake()
     {
-        slot = GameObject.FindGameObjectsWithTag("Slot");
-
-        
+        pads = GameObject.FindGameObjectsWithTag("Slot");
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Start()
     {
         Shuffle();
-        
     }
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    private void Shuffle()
-    {
-        List<Sprite> lstTemp = lstItem;
-        lstTemp.AddRange(lstItem);
 
-        for(int i=0; i< slot.Length; i++)
+    void Shuffle()
+    {
+        List<Sprite> duplicatedSprites = new List<Sprite>(spriteList);
+        duplicatedSprites.AddRange(spriteList);
+
+        for (int i = 0; i < duplicatedSprites.Count; i++)
         {
-            int rnd = Random.Range(0, lstTemp.Count);
-            SpriteRenderer target = slot[i].transform.Find("CatImage").GetComponent<SpriteRenderer>();
-            target.sprite = lstTemp[rnd];
-            lstTemp.RemoveAt(rnd);
-            target.enabled = true;
-            
+            Sprite temp = duplicatedSprites[i];
+            int randomIndex = Random.Range(i, duplicatedSprites.Count);
+            duplicatedSprites[i] = duplicatedSprites[randomIndex];
+            duplicatedSprites[randomIndex] = temp;
+        }
+
+        for (int i = 0; i < pads.Length; i++)
+        {
+            Image catImage = pads[i].transform.Find("CatImage").GetComponent<Image>();
+            if (catImage != null)
+            {
+                // Assegna lo sprite del retro
+                catImage.sprite = duplicatedSprites[i];
+
+                // Aggiungi l'event trigger per rilevare il click e collegarlo direttamente qui
+                EventTrigger trigger = pads[i].AddComponent<EventTrigger>();
+                EventTrigger.Entry entry = new EventTrigger.Entry();
+                entry.eventID = EventTriggerType.PointerClick;
+                entry.callback.AddListener((eventData) => { OnCardClicked(pads[i]); });
+                trigger.triggers.Add(entry);
+            }
+        }
+    }
+
+    void OnCardClicked(GameObject pad)
+    {
+        Image catImage = pad.transform.Find("CatImage").GetComponent<Image>();
+        if (catImage != null)
+        {
+            // Mostra l'immagine di CatImage
+            catImage.gameObject.SetActive(true);
+
+            // Nascondi il retro
+            GameObject backImage = catImage.transform.parent.gameObject;
+            if (backImage != null)
+            {
+                backImage.SetActive(false);
+            }
+
+            // Riproduci il suono del flip
+            audioSource.PlayOneShot(flipSound);
         }
     }
 }
