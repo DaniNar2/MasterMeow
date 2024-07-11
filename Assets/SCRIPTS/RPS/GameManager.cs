e10239447464e8ef5fbe9a0f04cf7c66812c8585
Dani <daniela.nardone106@gmail.com>
2024-07-11 23:41:15 +0200

    BEGINNING OF RPS (AGAIN) AND CLEANING
    This time RPS seems to work

---
+++ Assets/SCRIPTS/RPS/GameManager.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public enum Choices { ROCK, PAPER, SCISSORS, NONE }
+
+public class GameManager : MonoBehaviour
+{
+    const string win = " Wins!";
+    const string draw = "Draw!";
+    public static GameManager instance;
+    Choices playerChoice = Choices.NONE, opponentChoice = Choices.NONE;
+    bool isPlayerSelected, isOpponentSelected, isGameFinished, isOpponentAI;
+   string playerName, opponentName;
+   [SerializeField] Text winningMessageText;
+
+    // Update is called once per frame
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        playerName = "Player";
+        opponentName = "Opponent";
+        isOpponentAI = true;
+    }
+
+    public void Select(Choices myChoice, bool isPlayer)
+    {
+        if(isGameFinished)
+        {
+            return;
+        }
+        if(isPlayer)
+        {
+            playerChoice = myChoice;
+            isPlayerSelected = true;
+            if(isOpponentAI)
+            {
+                Select((Choices)Random.Range(0, 3), false);
+            }
+        }
+        else
+        {
+            opponentChoice = myChoice;
+            isOpponentSelected = true;
+        }
+        if(isPlayerSelected && isOpponentSelected)
+        {
+            isGameFinished = true;
+            DetermineWinner();
+        }
+        
+    }
+
+    public void DetermineWinner()
+    {
+        if(playerChoice == opponentChoice)
+        {
+            winningMessageText.text = draw;
+        }
+        else if(playerChoice == Choices.PAPER && opponentChoice == Choices.ROCK)
+        {
+            winningMessageText.text = playerName + win;
+        }
+        else if(playerChoice == Choices.ROCK && opponentChoice == Choices.SCISSORS)
+        {
+            winningMessageText.text = playerName + win;
+        }
+        else if(playerChoice == Choices.SCISSORS && opponentChoice == Choices.PAPER)
+        {
+            winningMessageText.text = playerName + win;
+        }
+        else if(playerChoice == Choices.PAPER && opponentChoice == Choices.SCISSORS)
+        {
+            winningMessageText.text = opponentName + win;
+        }
+        else if(playerChoice == Choices.ROCK && opponentChoice == Choices.PAPER)
+        {
+            winningMessageText.text = opponentName + win;
+        }
+        else if(playerChoice == Choices.SCISSORS && opponentChoice == Choices.ROCK)
+        {
+            winningMessageText.text = opponentName + win;
+        }
+    }
+}
