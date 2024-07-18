dca44829bf4ee25c702c2edd55c01ce8309db918
SofMel <sofia.melcarne16@gmail.com>
2024-07-18 12:26:17 +0200

    Ok, ora funziona

---
+++ Assets/SCRIPTS/Memory/Card.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    [HideInInspector]
+    public bool hasClicked, hasTurnFinished;
+
+    [SerializeField]
+    int index;
+
+    [HideInInspector]
+    public int cat;
+
+    [SerializeField]
+    Sprite unrevealed;
+
+    [SerializeField]
+    List<Sprite> cats;
+
+    SpriteRenderer renderer;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasClicked = false;
+        hasTurnFinished = false;
+        cat = MemoryManager.instance.myBoard.GetIndex(index);
+        renderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        renderer.sprite = unrevealed;
+    }
+
+    public void UpdateTurn()
+    {
+        hasClicked = true;
+        UpdateImage();
+        //animator.Play("Reveal", -1, 0f);
+    }
+
+    public void UpdateImage()
+    {
+        renderer.sprite = cats[cat];
+    }
+
+    public void RemoveTurn()
+    {
+        hasClicked = false;
+        RemoveImage();
+        //animator.Play("Unreveal", -1, 0f);
+    }
+
+    public void RemoveImage()
+    {
+        renderer.sprite = unrevealed;
+    }
+}
---
+++ Assets/SCRIPTS/Memory/MemoryManager.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryManager : MonoBehaviour
+{
+    public static MemoryManager instance;
+
+    public Board myBoard;
+
+    bool hasGameFinished, isFirstTurn;
+    Card first;
+
+    public void Replay()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        myBoard = new Board();
+        hasGameFinished = false;
+        isFirstTurn = true;
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            //if Game Has Finished
+            if (hasGameFinished) return;
+
+            //Raycase
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+            //if not hit
+            if (!hit.collider) return;
+
+            if(hit.collider.CompareTag("Card"))
+            {
+                if (hit.collider.gameObject.GetComponent<Card>().hasTurnFinished) return;
+
+                if(isFirstTurn)
+                {
+                    first = hit.collider.gameObject.GetComponent<Card>();
+                    first.UpdateTurn();
+                }
+                else
+                {
+                    Card second = hit.collider.gameObject.GetComponent<Card>();
+                    if(second.hasClicked)
+                    {
+                        first.RemoveTurn();
+                        second.RemoveTurn();
+                        isFirstTurn = !isFirstTurn;
+                        return;
+                    }
+                    second.UpdateTurn();
+
+                    if(first.cat == second.cat)
+                    {
+                        first.hasTurnFinished = true;
+                        second.hasTurnFinished = true;
+                        if(myBoard.UpdateChoice())
+                        {
+                            hasGameFinished = true;
+                            return;
+                        }
+                        isFirstTurn = !isFirstTurn;
+                        return;
+                    }
+
+                    first.RemoveTurn();
+                    second.RemoveTurn();
+                }
+                isFirstTurn = !isFirstTurn;
+            }
+        }
+    }
+}
